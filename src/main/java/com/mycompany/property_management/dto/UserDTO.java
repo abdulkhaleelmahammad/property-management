e2effe5854b8e149aba77b54c65c39ef46e4c9d1
Abdul Khaleel <khaleelmca102@gmail.com>
2025-12-31 22:46:28 +0530

    Field level exception handling

--- src/main/java/com/mycompany/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property_management/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.property_management.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +15,13 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Email id mandatory")
+    @NotEmpty(message = "Email id cannot be empty")
+    @Size(min=5, max=50, message = "Email id should be between 5 and 50 characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 
 }
